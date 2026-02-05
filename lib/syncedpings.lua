a62131a5f1ef23936b054b33854a4217f874cdff
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-02-04 22:30:18 -0500

    Undo SyncedPings rename

--- lib/syncedpings.lua
+++ lib/syncedpings.lua
@@ -1,30 +1,30 @@
----@class SyncedPing
-local SyncedPing = {}
-SyncedPing.ticks = 200
+---@class SyncedPings
+local SyncedPings = {}
+SyncedPings.ticks = 200
 
 ---@generic T
 ---@param pingFunc fun(...: T?)
 ---@param eventType "TICK"|"WORLD_TICK"
 ---@param ... T?
 ---@return fun(...: T?)
-function SyncedPing:new(pingFunc, eventType, ...)
+function SyncedPings:new(pingFunc, eventType, ...)
     self.pingFunc = pingFunc
     self.args = ...
-    if not SyncedPing[eventType] then
-        SyncedPing[eventType] = {}
+    if not SyncedPings[eventType] then
+        SyncedPings[eventType] = {}
         events[eventType]:register(function()
-            for i, sPing in ipairs(SyncedPing[eventType]) do
-                if world.getTime() % (SyncedPing.ticks + (i - 1)) == 0 then
+            for i, sPing in ipairs(SyncedPings[eventType]) do
+                if world.getTime() % (SyncedPings.ticks + (i - 1)) == 0 then
                     sPing.pingFunc(sPing.args)
                 end
             end
         end, "SyncedPings." .. eventType)
     end
-    table.insert(SyncedPing[eventType], self)
+    table.insert(SyncedPings[eventType], self)
     return function(...)
         self.args = ...
         self.pingFunc(...)
     end
 end
 
-return SyncedPing
+return SyncedPings
