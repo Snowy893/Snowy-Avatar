0ea88788d39a0a66847a0f4e922cb61ba3068f58
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-02-28 13:22:32 -0500

    Reorganized action wheel, low shield and shield fix are now separate

--- host.lua
+++ host.lua
@@ -1,15 +1,28 @@
 if not host:isHost() then return end
-
+--#region imports
 local util = require "lib.util"
 local syncedPings = require "lib.syncedpings"
-
-local page = action_wheel:newPage()
+--#endregion
 local sadChair = models.model.root.sadchair
-local hasSuperSecretShaders = client.compareVersions(client.getVersion(), "1.20.5") ~= 1
 local unlockCursorKey = "key.mouse.4" ---@type Minecraft.keyCode
-local offsetShield = config:load("shouldOffsetShield") or true
+
+for _, name in pairs(client.getActiveResourcePacks()) do
+    local n = name:lower()
+    if n:find("low") and n:find("shield") then
+        config:save("shouldFixShield", false)
+        config:save("shouldLowerShield", false)
+        break
+    end
+end
+
+local fixShield = config:load("shouldFixShield") or true
+local lowShield = config:load("shouldLowerShield") or true
 
 syncedPings.ticks = 4 * 20
+
+local page = action_wheel:newPage()
+local emotePage = action_wheel:newPage()
+local shieldPage = action_wheel:newPage()
 
 action_wheel:setPage(page)
 
@@ -27,37 +40,51 @@ function pings.creeper()
     animations.model.creeper:play()
 end
 
-if hasSuperSecretShaders then
-    page:newAction()
-        :title("Dithering")
-        :item("minecraft:apple")
-        :hoverColor(1, 0, 1)
-        :onToggle(function(toggle)
-            renderer:setPostEffect(toggle and "notch" or nil)
-        end)
-end
+util.switchPageAction(
+    page,
+    shieldPage,
+    "Shield",
+    "minecraft:shield"
+)
 
-page:newAction()
-    :title("Adjust Shield")
-    :item("minecraft:shield")
-    :hoverColor(1, 0, 1)
-    :toggled(true)
-    :onToggle(function(toggle)
-        offsetShield = toggle
-        config:save("shouldOffsetShield", toggle)
-    end)
+util.switchPageAction(
+    page,
+    emotePage,
+    "Emotes",
+    "minecraft:player_head"
+)
 
-page:newAction()
+emotePage:newAction()
     :title("Sad Chair")
     :item("minecraft:smooth_quartz_stairs")
     :hoverColor(1, 0, 1)
     :onToggle(syncedPings:new(pings.sadChair, false))
 
-page:newAction()
+emotePage:newAction()
     :title("Creeper")
     :item("minecraft:creeper_head")
     :hoverColor(1, 0, 1)
     :onLeftClick(pings.creeper)
+
+shieldPage:newAction()
+    :title("Adjust Shield")
+    :item("minecraft:shield")
+    :hoverColor(1, 0, 1)
+    :toggled(true)
+    :onToggle(function(toggle)
+        fixShield = toggle
+        config:save("shouldFixShield", toggle)
+    end)
+
+shieldPage:newAction()
+    :title("Low Shield")
+    :item("minecraft:magenta_glazed_terracotta")
+    :hoverColor(1, 0, 1)
+    :toggled(true)
+    :onToggle(function(toggle)
+        lowShield = toggle
+        config:save("shouldLowerShield", toggle)
+    end)
 
 keybinds:newKeybind("unlockCursor", unlockCursorKey)
     :onPress(function()
@@ -75,15 +102,19 @@ local taskLeft = itemLeftPart:newItem("left")
 
 function events.entity_init()
     function events.item_render(item, mode, pos, rot, scale, lefthanded)
-        if not offsetShield then return end
+        if not fixShield and not lowShield then return end
         if not mode:find("FIRST_PERSON") then return end
         if item:getUseAction() ~= "BLOCK" then return end
 
         local part = lefthanded and itemLeftPart or itemRightPart
         local task = lefthanded and taskLeft or taskRight
-        local offset = player:isUsingItem()
-            and (lefthanded and -4.5 or -6.24)
-            or  (lefthanded and -3.5 or -3.7)
+
+        local blocking = player:isUsingItem()
+        
+        local fixOffset = blocking and -1.74 or -0.2
+        local lowOffset = blocking and -4.5 or -3.5
+
+        local offset = ((fixShield and not lefthanded) and fixOffset or 0) + (lowShield and lowOffset or 0)
 
         task:setPos(0, offset, 0)
         task:setItem(item)
