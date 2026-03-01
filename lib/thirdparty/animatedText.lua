4e5d4fc512bddde1fa061794d5caaa6f2cf6064e
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-02-28 19:23:38 -0500

    New animated text version (i also added docs)

--- lib/thirdparty/animatedText.lua
+++ lib/thirdparty/animatedText.lua
@@ -1,89 +1,102 @@
---Animated Text API v0.0.1 (Snowy edited version)
+--Animated Text API v0.1.0 (docs added by Snowy)
+---@class AnimatedText
 local api = {}
 ---@type AnimatedTextTask[]
 local tasks = {}
 
----@param tbl table
+---@param tbl AnimatedTextCharacters
 local function trueLength(tbl)
-	local length = 0
-	for _, v in pairs(tbl) do
+	local length, _tbl = 0, {}
+	for k, v in pairs(tbl) do
 		for i = 1, #v.chars do
-			if #v.chars[i] <= 1 then length = length + client.getTextWidth(v.chars[i]) else length = length + 8 end
+			if #v.chars[i] <= 1 then
+				length = length + client.getTextWidth(v.chars[i])
+			else
+				length = length + 8
+			end
+			if v.chars[i] == "\n" or i == #v.chars and k >= #tbl then
+				table.insert(_tbl, length)
+				length = 0
+			end
 		end
 	end
-	return length
+	return _tbl
 end
 
 ---@param char string
 local function trueCharWidth(char)
-	if #char <= 1 then return client.getTextWidth(char) else return 8 end
+	if #char <= 1 or char == "\n" then return client.getTextWidth(char) else return 8 end
 end
 
----@param string string
----@param tbl table?
+---@param str string
 ---@return AnimatedTextCharacters[]
-local function deconstructString(string, tbl) --if tbl argument is provided, all characters will be appended to it, otherwise append to a separate table
-	local _tbl = tbl or {}
-	local escape, str, charsLeft, charIndex = false, '', 0, 0
-	for char in string:gmatch('[\x00-\x7F\xC2-\xF4][\x80-\xBF]*') do
-		if escape then str = str .. char else str = char end --look for emojis, if str is complete, add as char
-		if char == ':' then escape = not escape end
-		if char == ' ' and escape then
-			escape, charsLeft = false, #str - 1
-			for i = 1, #str do table.insert(_tbl, str:sub(i, i)) end
+local function deconstructString(str)
+	local match = "[\x00-\x7F\xC2-\xF4][\x80-\xBF]*"
+	local tbl, i, lastCapture, capturing = {}, 1, 1, false
+	for char in str:gmatch(match) do
+		local substring = str:sub(lastCapture, i)
+		if char == ":" then
+			if capturing then table.insert(tbl, substring) end
+			if not capturing and i == #str then table.insert(tbl, char) end
+			capturing, lastCapture = not capturing, i
+		elseif capturing and char:match("[%s]") or capturing and i == #str then
+			for _char in substring:gmatch(match) do table.insert(tbl, _char) end
+			capturing = false
+		elseif not capturing then
+			table.insert(tbl, char)
 		end
-		if not escape and charsLeft < 1 then table.insert(_tbl, str) end
-		charsLeft = charsLeft - 1
-		charIndex = charIndex + 1
+		i = i + #char
 	end
-	return {{raw = string, chars = _tbl}}
+	return tbl
 end
 
----@param json any
----@param r boolean?
-local function deconstructJson(json, r) --deconstruct json into separate characters with the appropriate components
-	local _json
-	if type(json) == "string" and not r then _json = parseJson(json) else _json = json end
-	local tbl =  not r and {} or nil
+---@param json table
+---@param _tbl table?
+---@return AnimatedTextCharacters[]
+local function deconstructJson(json, _tbl)
+	local tbl = _tbl or {}
 	---@class AnimatedTextCharacters
-	---@field raw string
-	---@field chars table
+	---@field chars string[]
 	---@field properties table
-	local charTbl = { chars = {} }
-	for k, v in pairs(_json) do
+	local charTbl = { chars = {}, properties = {} }
+	for k, v in pairs(json) do
 		if type(v) ~= "table" then
 			if k == "text" then
-				charTbl.raw = v
-				deconstructString(v, charTbl.chars)
+				charTbl.chars = deconstructString(v)
 			else
-				if not charTbl.properties then charTbl.properties = {} end
 				charTbl.properties[k] = v
 			end
 		else
-			deconstructJson(v, true) --recursive
+			deconstructJson(v, tbl)
 		end
 	end
-	if json.text then table.insert(tbl, charTbl) end --tables with nested objects inside will be ignored if that table doesn't include "text".
+	if json.text then table.insert(tbl, charTbl) end
 	return tbl
 end
 
 ---@param task AnimatedTextTask
----@param text table|string
-local function createTasks(task, text)
-	if type(text) == "table" then _text = deconstructJson(text) elseif type(text) == "string" then _text = deconstructString(text) end
-	local span, length = 0, trueLength(_text)
-	for _, v in ipairs(_text) do
+---@param _text string|table
+local function createTasks(task, _text)
+	local text = type(_text) == "table" and deconstructJson(_text) or
+	deconstructString(_text) ---@diagnostic disable-line: param-type-mismatch
+	local span, length, line = 0, trueLength(text), 1
+	for _, v in pairs(text) do
 		for _, char in pairs(v.chars) do
+			if char == "\n" then span, line = 0, line + 1 end
 			table.insert(task.textTasks, {
-				anchor = vec((span - length / 2) * -task.scale.x, 0, 0) + task.offset,
+				anchor = vec(
+					(span - length[line] / 2) * -task.scale.x,
+					(line - 1) * -task.scale.y * client.getTextHeight(""),
+					0
+				) + task.offset,
 				task = task.root:newText(#task.textTasks), ---@diagnostic disable-line: param-type-mismatch
 			})
-			local textTask = task.textTasks[#task.textTasks]
+			local textTask, _json = task.textTasks[#task.textTasks], { text = char }
+			for k, v in pairs(v.properties) do _json[k] = v end ---@diagnostic disable-line: redefined-local
 			textTask.task:pos(textTask.anchor)
 				:scale(task.scale)
-				:setAlignment('LEFT')
-                :text(v.properties and "{'text': '" .. char .. "', " .. toJson(v.properties):sub(2, -2) .. "}"
-					or char)
+				:setAlignment("LEFT")
+				:text(v.properties and toJson(_json) or char)
 			span = span + trueCharWidth(char)
 		end
 	end
@@ -92,7 +105,6 @@ end
 ---@param name string
 function api.remove(name)
 	for _, v in pairs(tasks[name].root:getTask()) do v:remove() end
-	tasks[name].textTasks = {}
 end
 
 ---@param name string
@@ -100,25 +112,22 @@ end
 ---@param offset Vector3
 ---@param scale Vector3
 ---@param parentType ModelPart.parentType
----@param json table|string
+---@param json string|table
 function api.new(name, parent, offset, scale, parentType, json)
-    if tasks[name] then api.remove(name) end
+	if tasks[name] then api.remove(name) end
 	---@class AnimatedTextTask
-	---@field offset Vector3
-	---@field scale Vector3
-	---@field root ModelPart
-	---@field textTasks { anchor: Vector3, task: TextTask }
-    tasks[name] = {
-        offset = offset,
-        scale = scale,
-        root = parent:newPart(name):setParentType(parentType),
-        textTasks = {},
+	tasks[name] = {
+		offset = offset,
+		scale = scale,
+		root = parent:newPart(name):setParentType(
+			parentType),
+		textTasks = {}
 	}
 	if json then createTasks(tasks[name], json) end
 end
 
 ---@param name string
----@param json table|string
+---@param json string|table
 function api.setText(name, json)
 	if tasks[name].textTasks then api.remove(name) end
 	createTasks(tasks[name], json)
