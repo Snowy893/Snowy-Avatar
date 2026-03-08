b0e3a91d466cc81d0b1bf23391638b08c1c42af4
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-03-08 05:30:40 -0400

    Using new util tick stuff :3

--- silly.lua
+++ silly.lua
@@ -1,17 +1,17 @@
 if not client.isModLoaded("sillyplugin") and not client.isModLoaded("goofyplugin") and not client.isModLoaded("extura") then return end
 
+local util = require "lib.util"
 local lift = require("lib.thirdparty.Lift")
 
 lift.config.enabled = true
 
 local blacklist, whitelistCache = {}, {} ---@type {[string]: boolean}, {[string]: boolean}
 
-function events.tick()
-    if world.getTime() % 60 ~= 0 then return end
+util.tick:register(function()
     for name, _ in pairs(world.getPlayers()) do
         if not blacklist[name] and not whitelistCache[name] then
             table.insert(lift.config.whitelist, name)
             whitelistCache[name] = true
         end
     end
-end
+end, 60)
